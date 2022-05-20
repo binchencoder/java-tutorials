a0984cd46716f069420663a5c2a45e6059f60467
chenbin <15811514091@163.com>
2022-05-20 22:58:17 +0800

    Add SystemTest

---
+++ src/test/java/com/binchencoder/study/systemprop/SystemTest.java
@@ -0,0 +1,20 @@
+package com.binchencoder.study.systemprop;
+
+import java.util.Properties;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class SystemTest extends TestCase {
+
+    @Test
+    public void test() {
+        String proVal = System.getProperty("file.separator");
+        System.out.println(proVal);
+    }
+
+    @Test
+    public void printSystemProps() {
+        Properties props = System.getProperties();
+    }
+
+}
