eed4f7bc6cb9deb4e4d719588cf511ae34f3dc24
chenbin <15811514091@163.com>
2022-06-09 10:46:15 +0800

    Add ForeachTest

---
+++ src/test/java/com/binchencoder/study/tests/ForeachTest.java
@@ -0,0 +1,48 @@
+package com.binchencoder.study.tests;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.testng.collections.Maps;
+
+public class ForeachTest {
+
+    @Test
+    public void testListForeach() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+
+        list.forEach(item -> {
+            if (item.equals(4)) {
+                return;
+            }
+            System.out.println(item);
+        });
+        System.out.println("end");
+    }
+
+    @Test
+    public void testMapForeach() {
+        Map<String, String> map = Maps.newHashMap();
+        map.put("1", "1");
+        map.put("2", "2");
+        map.put("3", "3");
+        map.put("4", "4");
+
+        map.forEach((String key, String val) -> {
+            if (key.equals("3")) {
+                return;
+            }
+            System.out.println("key: " + key + ", value: " + val);
+        });
+
+        System.out.println("end");
+    }
+}
